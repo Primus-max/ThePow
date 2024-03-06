33ecf47571bb61cf58785834d66517f10faa876a
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-03-06 23:30:24 +0300

    Добавьте файлы проекта.

---
+++ ThePow.cpp
@@ -0,0 +1,30 @@
+﻿// Написать рекурсивную функцию нахождения степени числа.
+
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int ThePow(int base, int exponent);
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int base = 0, exponent = 0;
+
+	cout << "Для возведения числа в степень, введи целочисленное значение основания и показатель степени";
+	cin >> base >> exponent;
+	cout << "\n";
+	cout << "Число " << base << " в степени " << exponent <<  " = " << ThePow(base, exponent);
+}
+
+int ThePow(int base, int exponent) {
+	if (exponent == 0)
+		return 1;
+
+	return base * ThePow(base, exponent - 1);
+
+}
